7d61e2734a192ffcb91d242f5e1b09919ddb401f
emily.deline <emily.deline@MU-C1-04.mullen.pvt.k12.co.us>
2019-11-14 11:54:54 -0700

    Should be finshed

---
+++ Credit Card Project/src/CreditCard.java
@@ -0,0 +1,66 @@
+public class CreditCard
+	{
+		static long validCard = (long) 5424180123456789.;
+		static long invalidCard = (long) 542418012345685.;
+		
+		static long[] creditCardNumbers = new long[16];
+		
+		static long card = validCard;
+		static long inCard = invalidCard;
+		
+		public static void main(String[] args)
+			{
+				strippingNumbers();
+				doublingNumbers();
+				originalDoubled();
+			}
+
+		private static void strippingNumbers()
+			{			
+					for (int c = creditCardNumbers.length - 1; c >= 0; c--)
+						{
+							long endingDigit = card % 10;	
+							card /= 10;
+							
+							creditCardNumbers[c] = endingDigit;
+
+						}
+								
+			}
+		
+		private static void doublingNumbers()
+			{
+				for (int dc = 0; dc < creditCardNumbers.length; dc += 2)
+					{
+						creditCardNumbers[dc] *= 2;
+						
+						if(creditCardNumbers[dc] >= 10)
+							{
+								long newDigg = (creditCardNumbers[dc] % 10) + (creditCardNumbers[dc] / 10);
+								creditCardNumbers[dc] = newDigg;
+							}
+
+					}
+				
+			}
+		
+		private static void originalDoubled()
+			{
+				int sum = 0;
+				
+				for (int adc = 0; adc < creditCardNumbers.length; adc++)
+					{
+						sum += creditCardNumbers[adc];
+					}
+				
+				if(sum % 10 == 0)
+					{
+						System.out.println("Your card is valid");
+					}
+				else
+					{
+						System.out.println("Your card is not valid. Good-bye.");
+					}
+			}
+
+	}
