d742839b5240253ec1687a054e49cd11d418e3b5
emily.deline <emily.deline@MU-C1-04.mullen.pvt.k12.co.us>
2019-11-20 12:23:37 -0700

    Okay now its really done and graded!!!

--- Credit Card Project/src/CreditCard.java
+++ Credit Card Project/src/CreditCard.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -5,31 +6,35 @@ import java.util.Scanner;
 public class CreditCard
 
 	{
-		static long cardNumber;
+		static long cardNumber; 
 		static long validCard = (long) 5424180123456789.;
 		static long invalidCard = (long) 542418012345685.;
 		
 		static long[] creditCardNumbers = new long[16];
 		
-		static long card = cardNumber;
-		static long inCard = cardNumber;
+		static long vcard = validCard;
+		static long inCard = invalidCard;
+		
+		static int counter = 0;
 		
 		public static void main(String[] args) throws IOException
 			{
 				Scanner file = new Scanner(new File("CreditCardNumbers"));
 
-			 while(file.hasNext())
-			  {
-				cardNumber = file.nextLong();
-				strippingNumbers();
-				doublingNumbers();
-				originalDoubled();
+			  while(file.hasNext())
+			   {	  
+				 cardNumber = file.nextLong();
+				 strippingNumbers();
+				 doublingNumbers();
+				 originalDoubled();
+				 countValidCard();
 				
-			  }
-			 
+			   }
+			  System.out.println("The number of valid cards are " + counter);
+			  System.out.println("The number of invalid cards are " + (99 - counter));
 				
 			}
-
+		
 		private static void strippingNumbers()
 			{			
 					for (int c = creditCardNumbers.length - 1; c >= 0; c--)
@@ -74,8 +79,25 @@ public class CreditCard
 					}
 				else
 					{
-						System.out.println("Your card is invalid. Good-bye.");
+						System.out.println("Your card is not valid. Good-bye.");
 					}
 			}
+		
+			
 
+		private static void countValidCard()
+			{
+				int sum = 0;
+				
+				for (long vc : creditCardNumbers)
+					{
+						sum += (int)vc;
+					}
+				if(sum % 10 == 0)
+					{
+						counter++;
+					}
+			}
+		
+		
 	}
